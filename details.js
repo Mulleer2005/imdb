c12d2dd845f8039dddcac513940c3e57288edd60
Marc Müller <mmuller@gnahs.com>
2024-06-14 13:07:10 +0200

    Formulari pr (WIP)

---
+++ details.js
@@ -0,0 +1,10 @@
+console.log(document.querySelector('.slider'))
+
+var slider = tns({
+    container: '.slider',
+    items: 2.5,
+    autoplay: true,
+    autoplayButtonOutput: false,
+    nav: false,
+    controlsContainer: '.slider-controls'
+  });
